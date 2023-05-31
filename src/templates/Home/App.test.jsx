faa6072324a49448ec6668b1377cc3b7f8c0d65f
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-05-31 16:44:49 -0300

    jest test and jest-styled-components config

--- src/templates/Home/App.test.jsx
+++ src/templates/Home/App.test.jsx
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import Home from '.';
+import { renderTheme } from '../Styles/render-theme';
+import { theme } from '../Styles/theme';
 
 test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { debug } = renderTheme(<Home />);
+  const headingContainer = screen.getByRole('heading', {
+    name: 'Olá',
+  }).parentElement;
+
+  expect(headingContainer).toHaveStyle({
+    background: theme.colors.mainBg,
+  });
+  expect(headingContainer).toMatchSnapshot();
+  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
