cb0bf741148c22681f4414758e694be6bf449115
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-05-31 15:40:54 -0300

    styled components start

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,11 @@
+import * as Styled from './styles';
+
 function Home() {
   return (
     <div className="App">
-      <h1>Olá</h1>
+      <Styled.Wrapper background="blue">
+        <h1>Olá</h1>
+      </Styled.Wrapper>
     </div>
   );
 }
