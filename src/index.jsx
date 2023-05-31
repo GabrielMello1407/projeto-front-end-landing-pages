d5025f3b83422c475da68c42444c689ef7fdcc95
Gabriel Mello Moraes <gabrielmellomoraes1407@gmail.com>
2023-05-31 15:16:49 -0300

    alterações basicas

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './templates/App';
-
+import Home from './templates/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
